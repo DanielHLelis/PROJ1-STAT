e3b3129847b94f97620603ba8041ec2edd669df2
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2022-10-20 20:49:57 -0300

    update demos

--- generate_demos.sh
+++ generate_demos.sh
@@ -13,12 +13,15 @@ cd datasets/demos
 rm ./*.json
 
 # Run Trials Here
-../../simulator-rust/target/release/simulator-rs 100000 100 0.01 20 50 0 
+../../simulator-rust/target/release/simulator-rs 100000 100 0.01 20 10 0 
 ../../simulator-rust/target/release/simulator-rs 100000 100 0.01 20 20 0 
 ../../simulator-rust/target/release/simulator-rs 100000 100 0.005 20 20 0
 ../../simulator-rust/target/release/simulator-rs 100000 100 0.005 20 20 0.0001
 ../../simulator-rust/target/release/simulator-rs 100000 100 0.005 20 50 0.0001
 ../../simulator-rust/target/release/simulator-rs 100000 100 0.005 20 20 0.001
+../../simulator-rust/target/release/simulator-rs 100000 100 0.005 100 999999 0.0001
+../../simulator-rust/target/release/simulator-rs 100000 100 0.005 500 999999 0.0001
+../../simulator-rust/target/release/simulator-rs 100000 100 0.005 20 999999 0.0001
 
 mv ./results/* .
 rmdir results
