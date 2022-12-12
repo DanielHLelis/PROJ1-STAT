b8856ae0cbf70b1a5982e4c6b30faabe235a858e
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2022-12-12 16:50:07 -0300

    idk what I've done

--- analysis/common/simulation.py
+++ analysis/common/simulation.py
@@ -1,6 +1,9 @@
 import os
 import json
+import time
 from collections import defaultdict
+
+import numpy as np
 
 from . import consts
 
@@ -67,9 +70,14 @@ def load_or_generate_simulation(
     salt: str = 'default',
     force_recreation: bool = False,
     max_cycles: int = 50000,
+    seed: Union[int, None] = None,
     datasets_path=consts.AUTO_GENERATED_DATASETS_DIR,
     simulator_cmd=consts.SIMULATOR_EXECUTABLE_PATH,
+    enable_rusty: bool = False,
 ) -> Union[SimulationResultsType, None]:
+    if seed is None:
+        seed = int(time.time() * 1000)
+
     file_name = f'{trial_count}-{n}-{p0}-{s}-{tr}-{beta}-{salt}.json'
     file_path = os.path.join(datasets_path, file_name)
 
@@ -78,11 +86,44 @@ def load_or_generate_simulation(
 
     # Generate new dataset (if necessary)
     if force_recreation or not os.path.isfile(file_path):
-        os.system(
-            f'SIM_MAX_CYCLES={int(max_cycles)} {simulator_cmd} {trial_count} {n} {p0} {s} {tr} {beta} > {file_path} 2> /dev/null')
+        if enable_rusty:
+            from rustysimulator import simulate
+            results = simulate(trial_count, n, p0, s0,
+                               tr, beta, seed, max_cycles)
+            with open(file_path, 'w') as f:
+                f.write(results)
+        else:
+            os.system(
+                f'SIM_MAX_CYCLES={int(max_cycles)} {simulator_cmd} {trial_count} {n} {p0} {s} {tr} {beta} > {file_path} 2> /dev/null')
 
     # Load and return simulation results
     return load_simulation_json(file_path)
+
+
+def sim_s_search(n: int, p: float, tr: int, beta: float, starting_s: int = 0, ending_s: int = 100, quantile: float = 0.01, target: int = 10000, trials: int = 1000, verbose: bool = False, **kwargs) -> Tuple[int, SimulationResultsType]:
+    simulations = {}
+    cur_simulation = None
+
+    s_low = starting_s
+    s_high = ending_s
+
+    while s_low < s_high:
+        cur_s = (s_high + s_low) // 2
+        if verbose:
+            print(f"Trying {cur_s}")
+        cur_simulation = load_or_generate_simulation(
+            trials, n, p, cur_s, tr, beta, max_cycles=target * 2, **kwargs)
+        simulations[cur_s] = cur_simulation
+        quantile_val = np.quantile(cur_simulation['results'], quantile)
+        if verbose:
+            print(f"{cur_s} quantile: {quantile_val}")
+
+        if quantile_val < target:
+            s_low = cur_s + 1
+        else:
+            s_high = cur_s
+
+    return s_low, simulations[s_low]
 
 
 def build_pmf(
